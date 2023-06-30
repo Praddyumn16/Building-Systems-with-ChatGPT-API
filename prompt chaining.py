a46db1e25e58a0421c38829100cfadac6e3c0682
Praddyumn Shukla <spraddyumn@gmail.com>
2023-06-30 20:39:08 +0530

    Create prompt chaining.py

---
+++ prompt chaining.py
@@ -0,0 +1,312 @@
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+############################################
+
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens, 
+    )
+    return response.choices[0].message["content"]
+
+############################################
+
+delimiter = "####"
+system_message = f"""
+You will be provided with customer service queries. \
+The customer service query will be delimited with \
+{delimiter} characters.
+Output a python list of objects, where each object has \
+the following format:
+    'category': <one of Computers and Laptops, \
+    Smartphones and Accessories, \
+    Televisions and Home Theater Systems, \
+    Gaming Consoles and Accessories, 
+    Audio Equipment, Cameras and Camcorders>,
+OR
+    'products': <a list of products that must \
+    be found in the allowed products below>
+
+Where the categories and products must be found in \
+the customer service query.
+If a product is mentioned, it must be associated with \
+the correct category in the allowed products list below.
+If no products or categories are found, output an \
+empty list.
+
+Allowed products: 
+
+Computers and Laptops category:
+TechPro Ultrabook
+BlueWave Gaming Laptop
+PowerLite Convertible
+TechPro Desktop
+BlueWave Chromebook
+
+Smartphones and Accessories category:
+SmartX ProPhone
+MobiTech PowerCase
+SmartX MiniPhone
+MobiTech Wireless Charger
+SmartX EarBuds
+
+Televisions and Home Theater Systems category:
+CineView 4K TV
+SoundMax Home Theater
+CineView 8K TV
+SoundMax Soundbar
+CineView OLED TV
+
+Gaming Consoles and Accessories category:
+GameSphere X
+ProGamer Controller
+GameSphere Y
+ProGamer Racing Wheel
+GameSphere VR Headset
+
+Audio Equipment category:
+AudioPhonic Noise-Canceling Headphones
+WaveSound Bluetooth Speaker
+AudioPhonic True Wireless Earbuds
+WaveSound Soundbar
+AudioPhonic Turntable
+
+Cameras and Camcorders category:
+FotoSnap DSLR Camera
+ActionCam 4K
+FotoSnap Mirrorless Camera
+ZoomMaster Camcorder
+FotoSnap Instant Camera
+
+Only output the list of objects, with nothing else.
+"""
+user_message_1 = f"""
+ tell me about the smartx pro phone and \
+ the fotosnap camera, the dslr one. \
+ Also tell me about your tvs """
+messages =  [  
+{'role':'system', 
+ 'content': system_message},    
+{'role':'user', 
+ 'content': f"{delimiter}{user_message_1}{delimiter}"},  
+] 
+category_and_product_response_1 = get_completion_from_messages(messages)
+print(category_and_product_response_1)
+
+'''
+OUTPUT:
+[{'category': 'Smartphones and Accessories'}, 
+{'category': 'Cameras and Camcorders'}, 
+{'category': 'Televisions and Home Theater Systems'}]
+'''
+
+############################################
+
+user_message_2 = f"""
+my router isn't working"""
+messages =  [  
+{'role':'system',
+ 'content': system_message},    
+{'role':'user',
+ 'content': f"{delimiter}{user_message_2}{delimiter}"},  
+] 
+response = get_completion_from_messages(messages)
+print(response)
+
+############################################
+
+
+# product information
+products = {
+    "TechPro Ultrabook": {
+        "name": "TechPro Ultrabook",
+        "category": "Computers and Laptops",
+        "brand": "TechPro",
+        "model_number": "TP-UB100",
+        "warranty": "1 year",
+        "rating": 4.5,
+        "features": ["13.3-inch display", "8GB RAM", "256GB SSD", "Intel Core i5 processor"],
+        "description": "A sleek and lightweight ultrabook for everyday use.",
+        "price": 799.99
+    },
+    "GameSphere VR Headset": {
+        "name": "GameSphere VR Headset",
+        "category": "Gaming Consoles and Accessories",
+        "brand": "GameSphere",
+        "model_number": "GS-VR",
+        "warranty": "1 year",
+        "rating": 4.6,
+        "features": ["Immersive VR experience", "Built-in headphones", "Adjustable headband", "Compatible with GameSphere X"],
+        "description": "Step into the world of virtual reality with this comfortable VR headset.",
+        "price": 299.99
+    },
+
+    "AudioPhonic Noise-Canceling Headphones": {
+        "name": "AudioPhonic Noise-Canceling Headphones",
+        "category": "Audio Equipment",
+        "brand": "AudioPhonic",
+        "model_number": "AP-NC100",
+        "warranty": "1 year",
+        "rating": 4.6,
+        "features": ["Active noise-canceling", "Bluetooth", "20-hour battery life", "Comfortable fit"],
+        "description": "Experience immersive sound with these noise-canceling headphones.",
+        "price": 199.99
+    },
+    "WaveSound Bluetooth Speaker": {
+        "name": "WaveSound Bluetooth Speaker",
+        "category": "Audio Equipment",
+        "brand": "WaveSound",
+        "model_number": "WS-BS50",
+        "warranty": "1 year",
+        "rating": 4.5,
+        "features": ["Portable", "10-hour battery life", "Water-resistant", "Built-in microphone"],
+        "description": "A compact and versatile Bluetooth speaker for music on the go.",
+        "price": 49.99
+    },
+    "AudioPhonic True Wireless Earbuds": {
+        "name": "AudioPhonic True Wireless Earbuds",
+        "category": "Audio Equipment",
+        "brand": "AudioPhonic",
+        "model_number": "AP-TW20",
+        "warranty": "1 year",
+        "rating": 4.4,
+        "features": ["True wireless", "Bluetooth 5.0", "Touch controls", "18-hour battery life"],
+        "description": "Enjoy music without wires with these comfortable true wireless earbuds.",
+        "price": 79.99
+    },
+    "FotoSnap DSLR Camera": {
+        "name": "FotoSnap DSLR Camera",
+        "category": "Cameras and Camcorders",
+        "brand": "FotoSnap",
+        "model_number": "FS-DSLR200",
+        "warranty": "1 year",
+        "rating": 4.7,
+        "features": ["24.2MP sensor", "1080p video", "3-inch LCD", "Interchangeable lenses"],
+        "description": "Capture stunning photos and videos with this versatile DSLR camera.",
+        "price": 599.99
+    },
+    "ActionCam 4K": {
+        "name": "ActionCam 4K",
+        "category": "Cameras and Camcorders",
+        "brand": "ActionCam",
+        "model_number": "AC-4K",
+        "warranty": "1 year",
+        "rating": 4.4,
+        "features": ["4K video", "Waterproof", "Image stabilization", "Wi-Fi"],
+        "description": "Record your adventures with this rugged and compact 4K action camera.",
+        "price": 299.99
+    },
+    "ZoomMaster Camcorder": {
+        "name": "ZoomMaster Camcorder",
+        "category": "Cameras and Camcorders",
+        "brand": "ZoomMaster",
+        "model_number": "ZM-CM50",
+        "warranty": "1 year",
+        "rating": 4.3,
+        "features": ["1080p video", "30x optical zoom", "3-inch LCD", "Image stabilization"],
+        "description": "Capture life's moments with this easy-to-use camcorder.",
+        "price": 249.99
+    }
+}
+
+############################################
+
+def get_product_by_name(name):
+    return products.get(name, None)
+
+def get_products_by_category(category):
+    return [product for product in products.values() if product["category"] == category]
+
+############################################
+
+print(get_product_by_name("TechPro Ultrabook"))
+print(get_products_by_category("Computers and Laptops"))
+print(user_message_1)
+print(category_and_product_response_1)
+
+############################################
+
+import json 
+
+def read_string_to_list(input_string):
+    if input_string is None:
+        return None
+
+    try:
+        input_string = input_string.replace("'", "\"")  # Replace single quotes with double quotes for valid JSON
+        data = json.loads(input_string)
+        return data
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON string")
+        return None   
+
+category_and_product_list = read_string_to_list(category_and_product_response_1)
+print(category_and_product_list)
+
+############################################
+
+def generate_output_string(data_list):
+    output_string = ""
+
+    if data_list is None:
+        return output_string
+
+    for data in data_list:
+        try:
+            if "products" in data:
+                products_list = data["products"]
+                for product_name in products_list:
+                    product = get_product_by_name(product_name)
+                    if product:
+                        output_string += json.dumps(product, indent=4) + "\n"
+                    else:
+                        print(f"Error: Product '{product_name}' not found")
+            elif "category" in data:
+                category_name = data["category"]
+                category_products = get_products_by_category(category_name)
+                for product in category_products:
+                    output_string += json.dumps(product, indent=4) + "\n"
+            else:
+                print("Error: Invalid object format")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    return output_string 
+
+product_information_for_user_message_1 = generate_output_string(category_and_product_list)
+print(product_information_for_user_message_1)
+
+############################################
+
+system_message = f"""
+You are a customer service assistant for a \
+large electronic store. \
+Respond in a friendly and helpful tone, \
+with very concise answers. \
+Make sure to ask the user relevant follow up questions.
+"""
+user_message_1 = f"""
+tell me about the smartx pro phone and \
+the fotosnap camera, the dslr one. \
+Also tell me about your tvs"""
+messages =  [  
+{'role':'system',
+ 'content': system_message},   
+{'role':'user',
+ 'content': user_message_1},  
+{'role':'assistant',
+ 'content': f"""Relevant product information:\n\
+ {product_information_for_user_message_1}"""},   
+]
+final_response = get_completion_from_messages(messages)
+print(final_response)
